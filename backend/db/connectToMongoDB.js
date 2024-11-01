0950d6c41b7a0361ad0ac3758cb6f841c36b55d5
Atharva K <atharva47kulkarni@gmail.com>
2024-11-01 15:32:51 +0530

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
-const VITE_MONGODB_URI =
-  "Your env var";
+
 const connectToMongoDB = async () => {
-  try {
-    await mongoose.connect(VITE_MONGODB_URI);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.log("Error connecting to MongoDB", error.message);
-  }
+	try {
+		await mongoose.connect(process.env.MONGO_DB_URI);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.log("Error connecting to MongoDB", error.message);
+	}
 };
 
 export default connectToMongoDB;
