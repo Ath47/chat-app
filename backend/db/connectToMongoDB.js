b7e06e7de8bc9a8b3f596a45051bdfb552f26ae3
Atharva K <atharva47kulkarni@gmail.com>
2024-11-01 15:28:03 +0530

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const VITE_MONGODB_URI =
-  "mongodb+srv://athuukul:8uiVLBX2WSW4UapO@cluster0.y1nyg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "Your env var";
 const connectToMongoDB = async () => {
   try {
     await mongoose.connect(VITE_MONGODB_URI);
