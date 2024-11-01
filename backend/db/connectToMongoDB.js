53f5d5b2ed2a2cf80c59d770c68983f77cd2b3b7
Ath47 <atharva47kulkarni@gmail.com>
2024-11-01 15:23:02 +0530

    Uploading all files

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
-
+const VITE_MONGODB_URI =
+  "mongodb+srv://athuukul:8uiVLBX2WSW4UapO@cluster0.y1nyg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const connectToMongoDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
-	}
+  try {
+    await mongoose.connect(VITE_MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error.message);
+  }
 };
 
 export default connectToMongoDB;
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -3,37 +3,29 @@ import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
-	const { loading, conversations } = useGetConversations();
-	return (
-		<div className='py-2 flex flex-col overflow-auto'>
-			{conversations.map((conversation, idx) => (
-				<Conversation
-					key={conversation._id}
-					conversation={conversation}
-					emoji={getRandomEmoji()}
-					lastIdx={idx === conversations.length - 1}
-				/>
-			))}
+  const { loading, conversations } = useGetConversations();
 
-			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
-		</div>
-	);
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      {conversations.map((conversation, idx) => (
+        <div
+          key={conversation._id}
+          style={{ animationDelay: `${idx * 0.1}s` }}
+          className="staggered-fade-in"
+        >
+          <Conversation
+            conversation={conversation}
+            emoji={getRandomEmoji()}
+            lastIdx={idx === conversations.length - 1}
+          />
+        </div>
+      ))}
+
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
+    </div>
+  );
 };
+
 export default Conversations;
-
-// STARTER CODE SNIPPET
-// import Conversation from "./Conversation";
-
-// const Conversations = () => {
-// 	return (
-// 		<div className='py-2 flex flex-col overflow-auto'>
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 		</div>
-// 	);
-// };
-// export default Conversations;
